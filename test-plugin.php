bf53993e508e466581c0f2dc506a2dd1f5e20df6
Paul Grotevant <pfg@utexas.edu>
2023-03-28 18:29:17 -0500

    Bump version

--- test-plugin.php
+++ test-plugin.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Test_Plugin
- * @version 1.0.0-alpha3
+ * @version 1.0.2
  */
 /*
 Plugin Name: UTexas Test Plugin
 Plugin URI: https://github.com/utexas-wp/test-plugin
 Description: This is a workflow test.
 Author: University of Texas at Austin
-Version: 1.0.0-alpha3
+Version: 1.0.2
 Author URI: https://its.utexas.edu
 GitHub Plugin URI: https://github.com/utexas-wp/test-plugin
 Primary Branch: main
