4a206b0ee4ffbddfb1280ff59d4b9ed3d979c57a
Paul Grotevant <pfg@utexas.edu>
2025-04-22 13:27:10 -0500

    Refactor for RYSE

--- test-plugin.php
+++ test-plugin.php
@@ -1,102 +1,25 @@
 <?php
+
+namespace RYSE\GitHubUpdaterDemo;
+
 /**
- * @package Test_Plugin
- * @version 1.0.7
+ * Plugin Name:        UTexas GitHub Updater Demo
+ * Plugin URI:         https://github.com/gravelpot/test-plugin-ryse
+ * Version:            1.0.0
+ * Description:        WordPress plugin to demonstrate how `GitHubUpdater` can enable WordPress to check for and update a custom plugin that's hosted in either a public or private repository on GitHub.
+ * Author:             Paul Grotevant
+ * Author URI:         https://gravelpot.org
+ * Text Domain:        test-plugin-ryse
+ * Tested up to:       6.8.1
+ * Requires at least:  6.5
+ * Requires PHP:       8.2
+ * Update URI:         https://github.com/gravelpot/test-plugin-ryse
+ * License:            GPLv2
+ * License URI:        https://www.gnu.org/licenses/gpl-2.0.html
  */
-/*
-Plugin Name: UTexas Test Plugin
-Plugin URI: https://github.com/utexas-wp/test-plugin
-Description: This is a workflow test.
-Author: University of Texas at Austin
-Version: 1.0.7
-Author URI: https://its.utexas.edu
-GitHub Plugin URI: https://github.com/utexas-wp/test-plugin
-Primary Branch: master
-*/
 
-function test_plugin_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, take her wrap, fellas
-Dolly, never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, golly, gee, fellas
-Have a little faith in me, fellas
-Dolly, never go away
-Promise, you'll never go away
-Dolly'll never go away again";
+if (!defined('ABSPATH')) exit;
 
-	// Here we split it into lines.
-	$lyrics = explode( "\n", $lyrics );
+require_once 'autoloader.php';
 
-	// And then randomly choose a line.
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
-}
-
-// This just echoes the chosen line, we'll position it later.
-function test_plugin() {
-	$chosen = test_plugin_get_lyric();
-	$lang   = '';
-	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
-		$lang = ' lang="en"';
-	}
-
-	printf(
-		'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
-		__( 'Quote from Hello Dolly song, by Jerry Herman:', 'hello-dolly' ),
-		$lang,
-		$chosen
-	);
-}
-
-// Now we set that function up to execute when the admin_notices action is called.
-add_action( 'admin_notices', 'test_plugin' );
-
-// We need some CSS to position the paragraph.
-function test_plugin_css() {
-	echo "
-	<style type='text/css'>
-	#dolly {
-		float: right;
-		padding: 5px 10px;
-		margin: 0;
-		font-size: 12px;
-		line-height: 1.6666;
-	}
-	.rtl #dolly {
-		float: left;
-	}
-	.block-editor-page #dolly {
-		display: none;
-	}
-	@media screen and (max-width: 782px) {
-		#dolly,
-		.rtl #dolly {
-			float: none;
-			padding-left: 0;
-			padding-right: 0;
-		}
-	}
-	</style>
-	";
-}
-
-add_action( 'admin_head', 'test_plugin_css' );
+new Plugin(__FILE__);
